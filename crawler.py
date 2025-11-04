1a8594be34a5449ae9ed8b8c1d8d09d4833065b1
Enzyklopaedie <mail.4.schule@gmail.com>
2025-11-04 20:42:55 +0100

    code is now more robust against malformed or unexpected game_url.txt file

--- crawler.py
+++ crawler.py
@@ -1,5 +1,4 @@
 import re
-
 import requests
 import stfmaster
 from main import website_to_scrape
@@ -19,23 +18,31 @@ def crawl_games(filename="game_urls.txt", game_id=10):
         else:
             game_urls.append(game.url)
             stfmaster.insert_item(game.url, filename)                      # storing the games to a file
-            print(f"ID: {game_id} erfolgreich hinzugefügt.")
+            print(f"ID: {game_id} was added successfully.")
             status_ok = 0
         game_id += 10
 
-# Extracting last item from file, getting its game id
-# and using it as seed for the next crawl process with the crawl_games() function
 
+# Extracting last item from file, getting its game id
+# and using it as seed for crawl_games()
 def get_start_game_id_for_next_crawl(filename="game_urls.txt"):
     remove_duplicates_from_games_file()
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             saved_urls = file.read().splitlines()
-            last_line = saved_urls[len(saved_urls) - 1]
-            extracted_game_id = int(re.search(r'/app/(\d+)', last_line).group(1))
-            # print("Last line is: ", last_line, "\nGame ID: ", extracted_game_id)
-            extracted_game_id = extracted_game_id + 10          # 10 is the default increment for giving out Game IDs
-                                                                # by Valve for games on Steam
+            try:
+                last_line = saved_urls[len(saved_urls) - 1]
+            except IndexError:
+                crawl_games(filename)
+                print("File empty. Starting new crawl..")
+            try:
+                extracted_game_id = int(re.search(r'/app/(\d+)', last_line).group(1))
+                print("Last line is: ", last_line, "\nGame ID: ", extracted_game_id, f"\nNew Start-ID for crawler: {extracted_game_id + 10}")
+                extracted_game_id = extracted_game_id + 10          # 10 is the default increment for giving out Game IDs
+                                                                    # by Valve for games on Steam
+            except AttributeError:
+                crawl_games(filename)
+                print("File not parsable. Starting new crawl..")
     except FileNotFoundError:
         print("No existing crawled urls found.")
         extracted_game_id = 10
@@ -55,9 +62,14 @@ def remove_duplicates_from_games_file(filename="game_urls.txt"):
 
 
 
-def crawling():
-    return
+def crawling(filename="game_urls.txt"):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            file.read().splitlines()
+        get_start_game_id_for_next_crawl(filename)
+        print("Continuing last crawl..")
+    except FileNotFoundError or AttributeError:
+        crawl_games(filename)
+        print("Starting new crawl..")
 
-# crawl_games()
-get_start_game_id_for_next_crawl()
-# remove_duplicates_from_games_file(filename="game_urls.txt")
+crawling("game_urls.txt")
